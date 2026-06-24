fa4bd2938a211bda13a9a0d5a8a451c3a54959da
Ale1519 <aurbizagastegui@gmail.com>
2026-06-23 20:02:12 -0500

    Add route for EditarVendedor component

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import RegistrarInspeccion from './pages/RegistrarInspeccion'
+import EditarVendedor from './pages/EditarVendedor'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/registro" element={<RegistrarInspeccion />} />
+        <Route path="/editar/:id" element={<EditarVendedor />} />
       </Routes>
     </Router>
   </React.StrictMode>,
